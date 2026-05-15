ea5de9ba7d5a1cfe5b7284ae465e76d123de8d8a
Omm3478 <bijaydash67@gmail.com>
2026-05-15 19:55:10 +0530

    Fix API URL for production backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
 
       const res = await axios.post(
 
-        "http://localhost:5000/api/auth/login",
+        "https://redditclonebyomm-1.onrender.com/login",
 
         formData
 
